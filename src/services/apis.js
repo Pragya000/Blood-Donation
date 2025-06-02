cd8e69b91d4bcb0b0ef352ec900fad31534e24c2
Pragya Khatwani <pragya@Pragyas-MacBook-Air.local>
2025-06-02 17:07:22 +0530

    Changes in api file

--- src/services/apis.js
+++ src/services/apis.js
@@ -1,5 +1,6 @@
 const BASE_URL = import.meta.env.VITE_BASE_URL;
 
+/*
 // Auth APIs
 export const SEND_OTP = `${BASE_URL}/auth/send-otp`;
 export const SIGNUP = `${BASE_URL}/auth/signup`;
@@ -43,3 +44,49 @@ export const CREATE_REGISTRATION = `${BASE_URL}/register/create`;
 export const LIST_USER_REGISTRATIONS = `${BASE_URL}/register/list-users`;
 export const LIST_HOSPITAL_REGISTRATIONS = `${BASE_URL}/register/list-hospitals`;
 export const UPDATE_REGISTRATION_STATUS = `${BASE_URL}/register/update`;
+*/
+
+
+// Auth APIs
+export const SEND_OTP = `${BASE_URL}/api/v1/auth/send-otp`;
+export const SIGNUP = `${BASE_URL}/api/v1/auth/signup`;
+export const LOGIN = `${BASE_URL}/api/v1/auth/login`;
+export const FORGOT_PASSWORD = `${BASE_URL}/api/v1/auth/forgot-password`;
+export const LOGOUT = `${BASE_URL}/api/v1/auth/logout`;
+export const VERIFY_CERTIFICATE = `${BASE_URL}/api/v1/auth/verify`;
+
+// User APIs
+export const GET_USER_DETAILS = `${BASE_URL}/api/v1/user/user-details`;
+export const GET_USER_PROFILE_DETAILS = `${BASE_URL}/api/v1/user/user-profile-details`;
+export const CREATE_USER_DETAILS = `${BASE_URL}/api/v1/user/create-user-details`;
+export const LIST_CERTIFICATES = `${BASE_URL}/api/v1/user/list-certificates`;
+
+// Hospital APIs
+export const CREATE_HOSPITAL_DETAILS = `${BASE_URL}/api/v1/hospital/create-hospital-details`;
+export const GET_HOSPITAL_REVIEWS = `${BASE_URL}/api/v1/hospital/get-hospital-reviews`;
+
+// Admin APIs
+export const GET_HOSPITALS_LIST = `${BASE_URL}/api/v1/admin/get-hospitals-list`;
+export const POPULATE_USER = `${BASE_URL}/api/v1/admin/populate-user`;
+export const CHANGE_HOSPITAL_STATUS = `${BASE_URL}/api/v1/admin/change-hospital-status`;
+export const GET_ADMIN_STATS = `${BASE_URL}/api/v1/admin/get-stats`;
+
+// Post APIs
+export const CREATE_POST = `${BASE_URL}/api/v1/posts/create-post`;
+export const GET_POSTS = `${BASE_URL}/api/v1/posts/get-posts`;
+export const GET_POST_DETAILS = `${BASE_URL}/api/v1/posts/get-post-details`;
+
+// Find APIs
+export const FIND_DONORS = `${BASE_URL}/api/v1/find/donors`;
+export const FIND_HOSPITALS = `${BASE_URL}/api/v1/find/hospitals`;
+
+// Request APIs
+export const CREATE_REQUEST = `${BASE_URL}/api/v1/request/create`;
+export const LIST_REQUESTS = `${BASE_URL}/api/v1/request/list`;
+export const UPDATE_REQUEST_STATUS = `${BASE_URL}/api/v1/request/update`;
+
+// Registration APIs
+export const CREATE_REGISTRATION = `${BASE_URL}/api/v1/register/create`;
+export const LIST_USER_REGISTRATIONS = `${BASE_URL}/api/v1/register/list-users`;
+export const LIST_HOSPITAL_REGISTRATIONS = `${BASE_URL}/api/v1/register/list-hospitals`;
+export const UPDATE_REGISTRATION_STATUS = `${BASE_URL}/api/v1/register/update`;
